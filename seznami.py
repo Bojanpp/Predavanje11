4214dbf9cad8be4973d4539d32e6e38c19a2768d
Bojan Joveski <bokicapk7@gmail.com>
2019-10-14 18:07:29 +0200

    Dopolnil seznam

--- seznami.py
+++ seznami.py
@@ -4,9 +4,29 @@ stevila = [5, 2, 8, 3]
 #izpis vseh stevil
 print(stevila)
 
+stevila.sort()
+print("Stevila smo uredili")
+print(stevila)
+
 #izpis stevila na mestu 1
 print(stevila[1])
 
 #izpis stevila na mestu 0
 print(stevila[0])
 
+#Count all elements
+print("Count of all elements is: " + str(len(stevila)))
+
+#Add elements in
+stevila.append(7)
+print("Dopolnjen seznam: ")
+print(stevila)
+
+#Delete elements
+stevila.remove(3)
+print("Spremenjen seznam: ")
+print(stevila)
+
+#Delete elements at specified index
+del stevila[0]
+print(stevila)
