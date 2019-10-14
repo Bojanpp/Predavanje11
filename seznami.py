3af30938dc49f1425eba2a9df2fd238241313a6f
Bojan Joveski <bokicapk7@gmail.com>
2019-10-14 17:45:20 +0200

    Dodal izpis enega stevila

--- seznami.py
+++ seznami.py
@@ -1,2 +1,12 @@
+#stetje    0 1 2 3 4 5 .....
 stevila = [5, 2, 8, 3]
+
+#izpis vseh stevil
 print(stevila)
+
+#izpis stevila na mestu 1
+print(stevila[1])
+
+#izpis stevila na mestu 0
+print(stevila[0])
+
